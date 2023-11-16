528f9a4757a7a2e59add691be6cc0688cd689f70
kabledev <kable.dev@gmail.com>
2023-11-17 00:49:01 +0300

    update

---
+++ app/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">about</router-link>
+    <h1>about</h1>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // nuxt.config.js
 
+import path from "path";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  rootDir: "./guide/",
+  rootDir: path.join(__dirname, "app/"),
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, "docs"),
+    },
+  },
   build: {},
   app: {},
 });
