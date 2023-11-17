2e48fe01d458eb066d2f0514f83a536c09f1cf2e
kabledev <kable.dev@gmail.com>
2023-11-17 06:29:24 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,6 @@ import path from "path";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   rootDir: path.join(__dirname, "app/"),
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, "docs"),
-    },
-  },
   app: {
     baseURL: process.env.NODE_ENV === "production" ? "/uipack/" : "/",
   },
